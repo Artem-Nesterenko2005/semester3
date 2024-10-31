c60ea4e0fd0d37bae3ef6331c63a9d9da20bb501
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-10-31 15:44:12 +0000

    Test1
    First version of the program

---
+++ Task1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+using Task1;
+
+namespace Task1
+{
+    /// <summary>
+    /// Program class for running.
+    /// </summary>
+    class Program
+    {
+        static private void Main()
+        {
+            Stopwatch stopwatch = new ();
+
+            stopwatch.Start();
+            var checkSum1 = MultiThreadCheck.ComputeToFilePath("C:\\Users\\Huawei\\Desktop\\3sem\\Task1\\MainDirectory");
+            stopwatch.Stop();
+            var resultMultiThread = stopwatch.Elapsed;
+
+            stopwatch.Restart();
+            var checkSum2 = SingleThreadCheck.ComputeToFilePath("C:\\Users\\Huawei\\Desktop\\3sem\\Task1\\MainDirectory");
+            stopwatch.Stop();
+            var resultSingleThread = stopwatch.Elapsed;
+
+            Console.WriteLine($"SingleThread time - {resultSingleThread}");
+            Console.WriteLine($"MultiThread time - {resultMultiThread}");
+        }
+    }
+}
---
+++ Task1/SingleThreadCheck.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    /// <summary>
+    /// Static class for single-thread md5 check-sum.
+    /// </summary>
+    static public class SingleThreadCheck
+    {
+        /// <summary>
+        /// Calculates the checksum of files and directories according to a given rule with single-thread mode.
+        /// </summary>
+        /// <param name="path">Path to file or directory.</param>
+        /// <returns>Check-sum for path.</returns>
+        /// <exception cref="InvalidDataException">Exception thrown when a path does not exist.</exception>
+        static public byte[] ComputeToFilePath(string path)
+        {
+            if (File.Exists(path)) 
+            {
+                var result = File.ReadAllBytes(path);
+                var fileHash = System.Security.Cryptography.MD5.HashData(result);
+                return fileHash;
+            }
+
+            if (Directory.Exists(path)) 
+            {
+                return ComputeDirectories(path);
+            }
+
+            throw new InvalidDataException("Incorrect path");
+        }
+
+        private static byte[] ComputeDirectories(string directory)
+        {
+            var listSums = new List<byte[]>();
+            var directories = Directory.GetDirectories(directory);
+            var files = Directory.GetFiles(directory);
+            Array.Sort(directories);
+            Array.Sort(files);
+            for (var i = 0; i < files.Length; ++i)
+            {
+                listSums.Add(ComputeToFilePath(files[i]));
+            }
+
+            for (var i = 0; i < directories.Length; ++i)
+            {
+                listSums.Add(ComputeDirectories(directories[i]));
+            }
+            return Encoding.ASCII.GetBytes(directory);
+        }
+    }
+}
