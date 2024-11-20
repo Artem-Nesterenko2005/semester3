ab1fe0007bc3097beca391118d572786c7fa19e3
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-11-20 20:17:37 +0000

    Add files via upload
    Add TestExamples

---
+++ MyNUnit/TestExamples/TestExamples.cs
@@ -0,0 +1,56 @@
+﻿// MIT License
+// Copyright (c) 2024 Artem-Nesterenko2005
+// All rights reserved
+
+using System;
+using Attributes;
+
+namespace TestExamples;
+
+public class Tests
+{
+    [Before]
+    public void Before()
+    {
+    }
+
+    [BeforeClass]
+    public static void BeforeClass()
+    {
+    }
+
+    [After]
+    public void After()
+    {
+    }
+
+    [AfterClass]
+    public static void AfterClass()
+    {
+    }
+
+    [Test]
+    public void TestPass()
+    {
+        string passed = string.Empty;
+    }
+
+    [Test(typeof(IndexOutOfRangeException))]
+    public void TestPassWithException()
+    {
+        throw new IndexOutOfRangeException();
+    }
+
+    [Test("ignore")]
+    public void TestIgnored()
+    {
+        string ignore = string.Empty;
+    }
+
+    [Test]
+    public void TestFail()
+    {
+        int[] array = new int[5] { 1, 2, 3, 4, 5 };
+        var fail = array[7];
+    }
+}
