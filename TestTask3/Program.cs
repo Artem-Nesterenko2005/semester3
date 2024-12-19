d3fc0148757e1212051f4ac2aa5fa830f6b9d9b9
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-12-19 15:58:32 +0000

    Test3

---
+++ TestTask3/Program.cs
@@ -0,0 +1,20 @@
+﻿// MIT License
+// Copyright (c) 2024 Artem-Nesterenko2005
+// All rights reserved
+
+namespace TestTask3;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        if (args.Length == 1)
+        {
+            await Server.ServerStart(int.Parse(args[0]));
+        }
+        else if (args.Length == 2)
+        {
+            await Client.ClientStart(int.Parse(args[0]), args[1]);
+        }
+    }
+}
---
+++ TestTask3/Server.cs
@@ -0,0 +1,57 @@
+﻿// MIT License
+// Copyright (c) 2024 Artem-Nesterenko2005
+// All rights reserved
+
+namespace TestTask3;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Class for server, listening to port.
+/// </summary>
+public static class Server
+{
+    /// <summary>
+    /// Launches the client, allowing correspondence.
+    /// </summary>
+    /// <param name="port">Port for connect.</param>
+    /// <returns>Return when client is closed.</returns>
+    public static async Task ServerStart(int port)
+    {
+        TextReader reader = Console.In;
+        TextWriter writer = Console.Out;
+
+        TcpListener listener = new (IPAddress.Any, port);
+        listener.Start();
+        using var socket = await listener.AcceptSocketAsync();
+
+        using var stream = new NetworkStream(socket);
+        using var streamReader = new StreamReader(stream);
+        using var streamWriter = new StreamWriter(stream) { AutoFlush = true };
+        while (true)
+        {
+            var message = await streamReader.ReadLineAsync();
+            if (message == "exit")
+            {
+                break;
+            }
+
+            await writer.WriteLineAsync(message);
+            var answer = reader.ReadLine();
+            await streamWriter.WriteLineAsync(answer);
+            await writer.WriteLineAsync(answer);
+            if (answer == "exit")
+            {
+                break;
+            }
+        }
+
+        socket.Close();
+    }
+}
