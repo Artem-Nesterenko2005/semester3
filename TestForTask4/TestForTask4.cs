4c0a4b0ae9243c42e56a87ff411b9156f2b65072
Artem-Nesterenko2005 <144232708+Artem-Nesterenko2005@users.noreply.github.com>
2024-12-26 09:32:28 +0000

    first version

---
+++ TestForTask4/TestForTask4.cs
@@ -0,0 +1,83 @@
+﻿// <copyright file="TestForTask4.cs" company="Artem-Nesterenko2005">
+// Copyright (c) Artem-Nesterenko2005. All Rights Reserved.
+// </copyright>
+// <license>
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// </license>
+
+namespace TestForTask4;
+using TestTask4;
+
+public class Tests
+{
+    private MultiThreadPriorityQueue<int> queue = new TestTask4.MultiThreadPriorityQueue<int>();
+
+    [Test]
+    public void TestPriorityEnqueue()
+    {
+        this.queue.Enqueue(1, 5);
+        this.queue.Enqueue(2, 3);
+        this.queue.Enqueue(3, 1);
+        this.queue.Enqueue(4, 9);
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(4));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(1));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(2));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(3));
+    }
+
+    [Test]
+    public void TestEnqueueMultiThread()
+    {
+        Thread[] threads = new Thread[5];
+        for (int i = 0; i < threads.Length; i++)
+        {
+            var localI = i;
+            threads[i] = new Thread(() =>
+            {
+                this.queue.Enqueue(threads.Length - localI, localI);
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(1));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(2));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(3));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(4));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(5));
+    }
+
+    [Test]
+    public void TestDequeueBeforeEnqueue()
+    {
+        Thread thread = new Thread(() =>
+        {
+            this.queue.Dequeue();
+        });
+        thread.Start();
+        this.queue.Enqueue(1, 1);
+        thread.Join();
+        Assert.That(this.queue.Size, Is.EqualTo(0));
+    }
+
+    [Test]
+    public void TestEqualPriorityEnqueue()
+    {
+        this.queue.Enqueue(5, 1);
+        this.queue.Enqueue(6, 1);
+        this.queue.Enqueue(7, 1);
+        this.queue.Enqueue(8, 1);
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(5));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(6));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(7));
+        Assert.That(this.queue.Dequeue(), Is.EqualTo(8));
+    }
+}
---
+++ TestTask4/MultiThreadPriorityQueue.cs
@@ -0,0 +1,119 @@
+﻿// <copyright file="MultiThreadPriorityQueue.cs" company="Artem-Nesterenko2005">
+// Copyright (c) Artem-Nesterenko2005. All Rights Reserved.
+// </copyright>
+// <license>
+// Licensed under the MIT License. See LICENSE in the repository root for license information.
+// </license>
+
+namespace TestTask4;
+
+/// <summary>
+/// Class for multithread priority queue with insert, delete and size check methods.
+/// </summary>
+/// <typeparam name="T">Data type for queue.</typeparam>
+public class MultiThreadPriorityQueue<T>
+{
+    private QueueElement<T>? head = null;
+
+    private object lockObject = new object();
+
+    private int size;
+
+    /// <summary>
+    /// Gets a value indicating whether queue has not elements.
+    /// </summary>
+    public bool Empty => this.head == null;
+
+    /// <summary>
+    /// Gets number of queue elements.
+    /// </summary>
+    public int Size => this.size;
+
+    /// <summary>
+    /// Method for adding data to a queue.
+    /// </summary>
+    /// <param name="data">Data added to queue.</param>
+    /// <param name="priority">Priority of added data.</param>
+    public void Enqueue(T data, int priority)
+    {
+        lock (this.lockObject)
+        {
+            if (this.Empty)
+            {
+                this.head = new QueueElement<T>(data, priority, null);
+                ++this.size;
+                Monitor.PulseAll(this.lockObject);
+                return;
+            }
+
+            if (this.head != null)
+            {
+                if (this.head.Priority < priority)
+                {
+                    QueueElement<T> node = new (data, priority, this.head);
+                    this.head = node;
+                    ++this.size;
+                    Monitor.PulseAll(this.lockObject);
+                    return;
+                }
+
+                var currentNode = this.head;
+                while (currentNode.Next != null && currentNode.Next.Priority >= priority)
+                {
+                    currentNode = currentNode.Next;
+                }
+
+                QueueElement<T> newNode = new (data, priority, currentNode.Next);
+                currentNode.Next = newNode;
+                ++this.size;
+                Monitor.PulseAll(this.lockObject);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method that returns and removes the top value from the queue.
+    /// </summary>
+    /// <returns>Data from top of queue.</returns>
+    /// <exception cref="EmptyPriorityQueueExceprion">Exception thrown when trying to remove an element from an empty queue.</exception>
+    public T? Dequeue()
+    {
+        lock (this.lockObject)
+        {
+            while (this.size == 0)
+            {
+                Monitor.Wait(this.lockObject);
+            }
+
+            T? data = this.head.Data;
+            this.head = this.head.Next;
+            --this.size;
+            return data;
+        }
+    }
+
+    private class QueueElement<T>
+    {
+        /// <summary>
+        /// Data from node.
+        /// </summary>
+        public T? Data;
+
+        /// <summary>
+        /// Priority from node.
+        /// </summary>
+        public int Priority;
+
+        /// <summary>
+        /// Next node of queue.
+        /// </summary>
+        public QueueElement<T>? Next;
+
+        public QueueElement(T? data, int priority, QueueElement<T>? next)
+        {
+            this.Data = data;
+            this.Priority = priority;
+            this.Next = next;
+        }
+    }
+}
